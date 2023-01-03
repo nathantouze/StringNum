ef993b12b3d8d13b57aa69bd640a0b0ed959842a
nathantouze <nathan.touze@epitech.eu>
2023-01-03 23:39:10 +0100

    rm useless scripts

--- compile.sh
+++
@@ -1,6 +0,0 @@
-rm -rf build
-mkdir build
-cd build
-cmake .. -G "Unix Makefiles"
-cmake --build .
-cp ./syracuse ../
--- compile_win.sh
+++
@@ -1,6 +0,0 @@
-rm -rf build
-mkdir build
-cd build
-cmake .. -G "Visual Studio 17 2022"
-cmake --build .
-cp ./Debug/StringNum ../
